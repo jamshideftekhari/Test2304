8ed842ce385fb9b7b57e3f37ac8d7ebac3602d63
jamshideftekhari <jame@ek.dk>
2026-04-23 13:47:03 +0200

    merge done

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+#add a route to return the current time
+from datetime import datetime
+
+@app.route('/time')
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
